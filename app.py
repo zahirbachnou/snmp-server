b20dac238b835aee80014a0808e97d7b3d0b586a
Zahir Bachnou <zahir.bachnou@protonmail.com>
2020-12-01 23:12:48 +0100

    Initialisation of the REST API.

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+api = Api(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+todos = {}
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 if __name__ == "__main__":
     app.run()
